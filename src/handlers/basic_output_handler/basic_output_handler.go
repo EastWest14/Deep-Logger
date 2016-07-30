ef134b9994333d5697efafb7e6cecf331a9a7a63
aprosikhin <aprosikhin@freewheel.tv>
2016-07-30 15:47:16 -0400

    Output handler seems to be working. All primary pieces are implemented, but can be very shaky.

--- src/handlers/basic_output_handler/basic_output_handler.go
+++ src/handlers/basic_output_handler/basic_output_handler.go
@@ -2,17 +2,23 @@ package basic_output_handler
 
 import (
 	"dispatcher"
+	"fmt"
 	"io"
 )
 
 type BasicOutputHandler struct {
 	DispatchLog       *dispatcher.DispatcherLog
 	OutputHandlerCode dispatcher.OutputHandlerCode
-	outputWriter      io.Writer
+	OutputWriter      io.Writer
 }
 
 func (boh *BasicOutputHandler) takeInEvent(ev dispatcher.Event) {
+	evString := fmt.Sprintln(string(ev.InputHandlerCode()) + " - " + ev.EventMessage())
+	boh.outputData([]byte(evString))
+}
 
+func (boh *BasicOutputHandler) outputData(data []byte) {
+	boh.OutputWriter.Write(data)
 }
 
 func (boh *BasicOutputHandler) RegisterWithDispatcher() {
--- src/handlers/basic_output_handler/basic_output_handler_test.go
+++ src/handlers/basic_output_handler/basic_output_handler_test.go
@@ -1,1 +1,38 @@
 package basic_output_handler
+
+import (
+	"dispatcher"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestTakeInEvent(t *testing.T) {
+	boh := BasicOutputHandler{DispatchLog: nil, OutputHandlerCode: "ABC", OutputWriter: os.Stdout}
+
+	ev := &EventDummy{inputHandlerCode: "XYZ", message: "Hello world!"}
+	boh.takeInEvent(ev)
+}
+
+type EventDummy struct {
+	inputHandlerCode dispatcher.InputHandlerCode
+	message          string
+	time             time.Time
+	evType           int
+}
+
+func (evD *EventDummy) InputHandlerCode() dispatcher.InputHandlerCode {
+	return evD.inputHandlerCode
+}
+
+func (evD *EventDummy) EventMessage() string {
+	return evD.message
+}
+
+func (evD *EventDummy) EventTime() time.Time {
+	return evD.time
+}
+
+func (evD *EventDummy) EventType() int {
+	return evD.evType
+}
